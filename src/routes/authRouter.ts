e13e576111f1b8ce4b5989888881031e69dace49
Mateusz Porębski <mateusz.porebski99@gmail.com>
2025-01-14 22:38:42 +0100

    remove unused code from auth controller/router, refactor the sign up controller

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -5,29 +5,9 @@ var router = express.Router();
 const authController = require('../controllers/authController')
 
 
-router.get('/login', authController.login_get)
-
-router.get('/signup', authController.signup_get)
-
-router.get('/verify', authController.verify_get)
-
-router.get('/verify/:verifyToken', authController.verifyToken_get)
-
-router.get('/successful-verification', authController.successfulVerification_get)
-
-router.get('/forgot-password', authController.forgotPassword_get)
-
-router.post('/forgot-password', authController.forgotPassword_post)
-
-router.get('/forgot-password/:verifyToken', authController.forgotPasswordWithToken_get)
-
-router.post('/forgot-password/:verifyToken', authController.forgotPasswordWithToken_post)
-
 router.post('/login', authController.login_post)
 
 router.post('/signup', authController.signup_post)
-
-router.get('/logout', authController.logout_get)
 
 router.get('/current_user', authController.currentUser_get)
 
